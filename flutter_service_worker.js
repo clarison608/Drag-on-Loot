dd93fa015fee8aa167dad080d204d7dea8846c43
Brandon Chandler <bchandler608@gmail.com>
2025-05-15 02:14:35 -0400

    Think of somting punny or else

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -407,7 +407,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "38f37611c09f1999edf056a9967b40c7
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "a11482e5ca81559e4e68363a788002f4",
+"flutter_bootstrap.js": "fa4b3824abd04d3e98680984be0b3a37",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
