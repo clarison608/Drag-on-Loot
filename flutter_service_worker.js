5a80b830f7685ca00a724430707cf69494ed38a6
Brandon Chandler <bchandler608@gmail.com>
2025-05-15 04:38:27 -0400

    Debug time, not even error's what do
    Cry? (\ .,. /)
    
    Its like 5 AM go to bed! ?/\?

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -408,14 +408,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "b9e203beb397542db548ad2efea1b10d
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "0e03a5e46e82aad069fc3ee5b3c66e13",
+"flutter_bootstrap.js": "f788f44d5d435623b362daaa4e600840",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "ce97efff280b7691667682c5a9507f39",
 "/": "ce97efff280b7691667682c5a9507f39",
-"main.dart.js": "1cb2c610c52efb96bed218af99be37e8",
+"main.dart.js": "0aa00077e15f6f6b3b309ff80708cf8f",
 "manifest.json": "3c54f02946a53f12430fd4208bbec055",
 "version.json": "a47fe4106142bc1bd6e68496b1cf948b"};
 // The application shell files that are downloaded before a service worker can
