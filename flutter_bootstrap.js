0e6f9f25fb6bb93c5c72d487b16c5cde7c7889f6
Brandon Chandler <bchandler608@gmail.com>
2025-05-15 00:58:09 -0400

    Initialization fault fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "284302862"
+    serviceWorkerVersion: "1390444391"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -407,14 +407,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "38f37611c09f1999edf056a9967b40c7
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "2a4123d2979600fc69e1a7ed7c5a4f49",
+"flutter_bootstrap.js": "55f87dcb7dac71a6deac180901e0eb8f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "281502954693810f43cda37ca89176a7",
 "/": "281502954693810f43cda37ca89176a7",
-"main.dart.js": "5fe955d4ee66e56a416ee16dae1508b2",
+"main.dart.js": "2d4aaa7f727e6893dfb5ce549baf6ee4",
 "manifest.json": "3c54f02946a53f12430fd4208bbec055",
 "version.json": "a47fe4106142bc1bd6e68496b1cf948b"};
 // The application shell files that are downloaded before a service worker can
