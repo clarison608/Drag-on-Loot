22205bd220a3333f11558928f43c0148865bc196
Brandon Chandler <bchandler608@gmail.com>
2025-05-15 00:46:18 -0400

    version 0.3 first publish

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2777722866"
+    serviceWorkerVersion: "284302862"
   }
 });
