06176384d528d9df7566c6b5fb03d43591ee6e40
Brandon Chandler <bchandler608@gmail.com>
2025-05-15 04:21:57 -0400

    It was this many pushes until I relized I did not need to do all the work I had been doing
    Feelz bad man (\ .,. /)
    
    Think of it as a learning exercise ?/\?

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -408,7 +408,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b9e203beb397542db548ad2efea1b10d
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "d9deceae2e6cd91ce2b66044be5aeecb",
+"flutter_bootstrap.js": "0e03a5e46e82aad069fc3ee5b3c66e13",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
