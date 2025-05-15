a4a0df9ccf0c47dd6f91548994b2c347ccd40ebc
Brandon Chandler <bchandler608@gmail.com>
2025-05-15 01:39:38 -0400

    some number over the rainbow

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1992542949"
+    serviceWorkerVersion: "2380089350"
   }
 });
