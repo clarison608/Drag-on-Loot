e3f48f68d7b535623df7ccea754a3ea7d2e7f5e2
Brandon Chandler <bchandler608@gmail.com>
2025-05-15 03:20:08 -0400

    Of course I did not get Firebase setup first try
    You ever feel like your complicating things too much ?/\?
    
    Nah it needs this or else we cant do fun things later (\ .,. /)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -408,14 +408,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "b9e203beb397542db548ad2efea1b10d
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "368848f6b84f94a3b475038ffef69b36",
+"flutter_bootstrap.js": "a0016072c2968b45782cc7d1db5b709a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "ce97efff280b7691667682c5a9507f39",
 "/": "ce97efff280b7691667682c5a9507f39",
-"main.dart.js": "6b6ac9cef4e84e83b1d297257bda1188",
+"main.dart.js": "565a7adb4cd0bdbf7e02e91ad032187f",
 "manifest.json": "3c54f02946a53f12430fd4208bbec055",
 "version.json": "a47fe4106142bc1bd6e68496b1cf948b"};
 // The application shell files that are downloaded before a service worker can
