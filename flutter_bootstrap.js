c17d2e77ac7a232a48049aff40748390601dc9ab
Brandon Chandler <bchandler608@gmail.com>
2025-05-15 06:44:55 -0400

    asdf
    asdf indeed ?/\?

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2264302704"
+    serviceWorkerVersion: "3926093568"
   }
 });
