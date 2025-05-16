f22a630f80082035449dd90139f757fa0f90d37d
Brandon Chandler <bchandler608@gmail.com>
2025-05-15 23:53:25 -0400

    going to sleep on time tonight
    Doubt (\ .,. /)
    
    Doubt ?/\?

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -408,14 +408,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "b9e203beb397542db548ad2efea1b10d
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "7381305a40ff77c748a886ed22e0e7ed",
+"flutter_bootstrap.js": "976c5299b289252acb356f3a8e9a0c08",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "ce97efff280b7691667682c5a9507f39",
 "/": "ce97efff280b7691667682c5a9507f39",
-"main.dart.js": "46405cd81a3f461b73159e499ea7b42d",
+"main.dart.js": "e214f3c615570da8ae02a69abac79512",
 "manifest.json": "3c54f02946a53f12430fd4208bbec055",
 "version.json": "a47fe4106142bc1bd6e68496b1cf948b"};
 // The application shell files that are downloaded before a service worker can
