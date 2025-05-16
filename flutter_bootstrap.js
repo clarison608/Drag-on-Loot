35aeaee000bc995440654d25166e9133b462ca2b
Brandon Chandler <bchandler608@gmail.com>
2025-05-15 23:24:33 -0400

    mananana

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2762365561"
+    serviceWorkerVersion: "1081413873"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -408,7 +408,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b9e203beb397542db548ad2efea1b10d
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "8d3df4e1834a3020ebc7c3b006695b41",
+"flutter_bootstrap.js": "7381305a40ff77c748a886ed22e0e7ed",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
